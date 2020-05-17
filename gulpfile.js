047bdadeba00813240cc3b1fbea227dee1fb7e8c
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-18 00:27:59 +0600

    Gulp and assets
    #gulp version 3.9/4+  won't work with node version 12+ so
    node v10.18.0/ npm  6.14.5  and gulp  3.9.1  version used

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+
+    // combined result stored in public folder 
+
+    .styles([
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        // 'libs/bootstrap.min.css',
+        'libs/font-awesome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css',
+        // 'libs/styles.css'
+
+    ], './public/css/libs.css')
+
+    .scripts([
+        'libs/bootstrap.js',
+        // 'libs/bootstrap.min.js',
+        'libs/jquery.js',
+        'libs/metisMenu.js',
+        'libs/sb-admin-2.js',
+        'libs/scripts.js'
+
+    ], './public/js/libs.js')
+
+
+
+
+
+
+
 });
