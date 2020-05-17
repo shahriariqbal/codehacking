718c6e02c143e9074aeb9d4a223a31d910184631
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-18 00:43:26 +0600

    Admin master file - download file
    # copied admin.blade.php file
    # admin frontend
    #added /admin route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,4 +8,10 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('/admin', function () {
+
+    return view('admin.index');
+    
+});
+
 Route::resource('admin/users', 'AdminUsersController');
