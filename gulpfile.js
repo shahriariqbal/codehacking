994637218a4e4742048d1ca63dbacbdf98f472c9
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-18 00:56:25 +0600

    Version control and modifying master page
    # edit frontend (admin-page)

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,31 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix.sass('app.scss')
+    mix
+        .sass('app.scss')
+        // combined result stored in public folder
 
-    // combined result stored in public folder 
-
-    .styles([
-        'libs/blog-post.css',
-        'libs/bootstrap.css',
-        // 'libs/bootstrap.min.css',
-        'libs/font-awesome.css',
-        'libs/metisMenu.css',
-        'libs/sb-admin-2.css',
-        // 'libs/styles.css'
-
-    ], './public/css/libs.css')
-
-    .scripts([
-        'libs/bootstrap.js',
-        // 'libs/bootstrap.min.js',
-        'libs/jquery.js',
-        'libs/metisMenu.js',
-        'libs/sb-admin-2.js',
-        'libs/scripts.js'
-
-    ], './public/js/libs.js')
-
-
-
-
-
-
-
+    .styles(
+            [
+                'libs/blog-post.css',
+                'libs/bootstrap.css',
+                // 'libs/bootstrap.min.css',
+                'libs/font-awesome.css',
+                'libs/metisMenu.css',
+                'libs/sb-admin-2.css'
+                // 'libs/styles.css'
+            ],
+            './public/css/libs.css'
+        )
+        .scripts(
+            [
+                'libs/bootstrap.js',
+                // 'libs/bootstrap.min.js',
+                'libs/jquery.js',
+                'libs/metisMenu.js',
+                'libs/sb-admin-2.js',
+                'libs/scripts.js'
+            ],
+            './public/js/libs.js'
+        );
 });
