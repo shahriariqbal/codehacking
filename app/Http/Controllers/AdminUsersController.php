ea46fa2e01c74bff1fdc61d96b346dbc4f8129fd
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-18 16:49:16 +0600

    Displaying users
    # display users at admin/users route

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\User;
+
 class AdminUsersController extends Controller
 {
     /**
@@ -15,7 +17,9 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1> Users </h1>
+
+
+<table class="table">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Created</th>
+        <th>Updated</th>
+      </tr>
+    </thead>
+    <tbody>
+
+        @if($users)
+           @foreach ($users as $user)
+
+           <tr>
+           <td> {{$user->id }}</td>
+           <td> {{$user->name }}</td>
+           <td> {{$user->email }}</td>
+           <td>{{$user->created_at->diffForHumans()}}</td>
+           <td> {{$user->updated_at->diffForHumans()}}</td>
+          </tr>
+               
+           @endforeach
+            
+        @endif
+
+
+
+
+
+    </tbody>
+  </table>
+    
+@stop
