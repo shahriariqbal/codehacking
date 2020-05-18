dcaeaf5b1815b07ae4705a22b192018e512c1462
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-18 18:16:49 +0600

    Laravel collective html package
    # followed  https://laravelcollective.com/docs/5.2/html
    # create form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,5 +3,20 @@
 @section('content')
 
 <h1> Create Users </h1>
+
+ {!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store']) !!}
+
+ <div class="form-group">
+     {!! Form::label('title', 'Title:') !!}
+     {!! Form::text('title', null, ['class'=> 'form-control'])  !!}
+
+ </div>
+
+ <div class="form-group">
+     {!! Form::submit('Create Post', ['class' => 'btn btn-primary']) !!}
+
+ </div>
+
+{!! Form::close() !!}
     
 @stop
