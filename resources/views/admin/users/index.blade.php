28da654ecca1cce6a59a9561cd63140f9273c496
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-18 17:47:53 +0600

    Create page
    # build  create user at  admin/users/create route

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -11,6 +11,8 @@
         <th>Id</th>
         <th>Name</th>
         <th>Email</th>
+        <th>Role</th>
+        <th>Active</th>
         <th>Created</th>
         <th>Updated</th>
       </tr>
@@ -24,6 +26,8 @@
            <td> {{$user->id }}</td>
            <td> {{$user->name }}</td>
            <td> {{$user->email }}</td>
+           <td> {{$user->role->name }}</td>
+           <td> {{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
            <td>{{$user->created_at->diffForHumans()}}</td>
            <td> {{$user->updated_at->diffForHumans()}}</td>
           </tr>
