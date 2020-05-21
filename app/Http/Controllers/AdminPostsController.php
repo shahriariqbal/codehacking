538c664a217c3c8fe8043501a219b2ce4a73a1da
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-21 14:03:32 +0600

    Posts - Displaying and creating posts with categories

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -31,7 +32,8 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::lists('name', 'id')->all();
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -27,7 +27,7 @@
            <td> {{$post->id}}</td>
            <td>  <img height="50" width="50" src="{{ $post->photo ? $post->photo->file : 'http://placehold.it/50x50'}}" alt="" class="img-responsive img-rounded"> </td>
            <td> {{$post->user->name}} </td>
-           <td> {{$post->category_id}} </td>
+           <td> {{$post->category ? $post->category->name : 'Uncategorized'}} </td>
            <td> {{$post->title}}</td>
            <td> {{$post->body}} </td>
            <td> {{$post->created_at->diffForHumans()}}</td>
