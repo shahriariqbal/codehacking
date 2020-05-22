65748e2716cfef51fb96450098ec44cf4471e347
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-23 01:49:59 +0600

    Comments - Setting up
    # comments, replies route
    # comments view
    # replies view
    # comments migration
    # replies migration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,6 +20,9 @@ Route::group(['middleware' => 'admin'], function () {
     Route::resource('admin/posts', 'AdminPostsController');
     Route::resource('admin/categories', 'AdminCategoriesController');
     Route::resource('admin/media', 'AdminMediasController');
+
+    Route::resource('admin/comments', 'PostCommentsController');
+    Route::resource('admin/comment/replies', 'CommentRepliesController');
         
 });
 
