c9a46112daeec674eaf356d6e06d7c8df13e26bb
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-23 02:18:08 +0600

    Comments - Relations and mass assignment
    # Comment model
    # CommentReply model
    # created PostCommentController  controller
    # created CommentRepliesController  controller

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'post_id',
+        'author',
+        'email',
+        'body',
+        'is_active'
+    ];
+
+    public function replies(){
+        return $this->hasMany('App\CommentReply');
+    }
 }
--- app/CommentReply.php
+++ app/CommentReply.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class CommentReply extends Model
 {
-    //
+    protected $fillable = [
+        'comment_id',
+        'author',
+        'email',
+        'body',
+        'is_active'
+    ];
+
+    public function comment(){
+        return $this->belongsTo('App\Comment');
+    }
 }
