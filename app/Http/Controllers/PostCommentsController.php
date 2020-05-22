95ce2a901e8292555bb4cb335262ee0528d1cc1d
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-23 03:22:04 +0600

    Comments - Setting up views
    # comments  view
    # comment navigation added in  main layout view
    # PostCommentsController-> index

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-class PostCommentController extends Controller
+class PostCommentsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,7 +15,7 @@ class PostCommentController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.comments.index');
     }
 
     /**
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -146,6 +146,10 @@
                                 <a href=" {{ route('admin.posts.create') }} ">Create Post</a>
                             </li>
 
+                            <li>
+                                <a href=" {{ route('admin.comments.index') }} ">All Comments</a>
+                            </li>
+
                         </ul>
                         <!-- /.nav-second-level -->
                     </li>
@@ -181,6 +185,7 @@
                         </ul>
                         <!-- /.nav-second-level -->
                     </li>
+
 
 
 
