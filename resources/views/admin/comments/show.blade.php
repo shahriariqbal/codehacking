6ac8c2ff730d2477b4d2532aec10536b514759dd
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-23 21:39:48 +0600

    Comments - Displaying comments

---
+++ resources/views/admin/comments/show.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.admin')
+
+@section('content')
+
+     @if ($comments)
+         
+    <table style="text-align:center" class="table table-bordered table-hover">
+        <thead >
+            <tr>
+                <th style="text-align:center">Id</th>
+                <th style="text-align:center">Author</th>
+                <th style="text-align:center">Email</th>
+                <th style="text-align:center">Body</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+                <tr>
+                    <td>{{$comment->id }}</td>
+                    <td>{{$comment->author }}</td>
+                    <td>{{$comment->email }}</td>
+                    <td>{{$comment->body }}</td>
+                    <td> <a href="{{ route('home.post', $comment->post->id ) }}"> View Post </a></td>
+
+                    <td>
+                        @if ($comment->is_active == 1)
+                            {!! Form::open(['method'=>'PATCH', 'action'=>[ 'PostCommentsController@update', $comment->id ]]) !!}
+                            <input type="hidden" name="is_active" value="0">
+                            <div class="form-group">
+                                {!! Form::submit('Un-approve', ['class'=>'btn btn-success']) !!}
+                            </div>
+                            {!! Form::close() !!}
+                        @else
+                            {!! Form::open(['method'=>'PATCH', 'action'=>[ 'PostCommentsController@update', $comment->id ]]) !!}
+                            <input type="hidden" name="is_active" value="1">
+                            <div class="form-group">
+                                {!! Form::submit('Approve', ['class'=>'btn btn-info']) !!}
+                            </div>
+                            {!! Form::close() !!}
+                        @endif
+                    </td>
+
+                    <td> 
+                        {!! Form::open(['method'=>'DELETE', 'action'=>[ 'PostCommentsController@destroy', $comment->id ]]) !!}
+                        <div class="form-group">
+                            {!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+                        </div>
+                        {!! Form::close() !!}
+                    </td>
+
+
+
+
+                </tr>
+
+        </tbody>
+    </table>
+
+ @endif
+
+ @else
+        <h1 class="text-center"> No Comments</h1>
+
+
+@stop
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -30,6 +30,8 @@
            <td> {{$post->category ? $post->category->name : 'Uncategorized'}} </td>
            <td> {{$post->title}}</td>
            <td> {{str_limit(  $post->body, 30) }} </td>
+           <td><a href="{{route('home.post', $post->id)}}"> View Post</a></td>
+           <td><a href="{{route('admin.comments.show', $post->id)}}"> View Comments</a></td>
            <td> {{$post->created_at->diffForHumans()}}</td>
            <td> {{$post->updated_at->diffForHumans()}}</td>
           </tr>
