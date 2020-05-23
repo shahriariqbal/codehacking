498ed692857b0e4128c0e48da8643de28027a99e
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-24 03:37:21 +0600

    Comments - Displaying replies

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -84,16 +84,23 @@
                 </h4>
                 <p> {{ $comment->body }} </p>
 
+
+                @if (count($comment->replies)> 0)
+
+                @foreach ($comment->replies as $reply)
+                    
+               
+
            <!-- Nested Comment -->
-            <div class="media">
+            <div id="nested-comment" class="media">
                 <a class="pull-left" href="#">
-                    <img class="media-object img-rounded" src="http://placehold.it/64x64" alt="">
+                <img height="34" class="media-object img-rounded" src=" {{ $reply->photo }}" alt="">
                 </a>
                 <div class="media-body">
-                    <h4 class="media-heading">Nested Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
+                    <h4 class="media-heading"> {{$reply->author}}
+                    <small> {{$reply->created_at->diffForHumans()}} </small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <p> {{$reply->body}} </p>
                 </div>
 
 
@@ -112,6 +119,10 @@
 
             </div>
             <!-- End Nested Comment -->
+
+            @endforeach
+
+            @endif
 
 
             </div>
