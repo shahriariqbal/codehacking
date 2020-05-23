6e4dcbebbe4fcb825167bdb84b3421a55d318195
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-23 18:11:19 +0600

    Comments - Creating a comment

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
 
 class PostCommentsController extends Controller
 {
@@ -36,7 +38,17 @@ class PostCommentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = Auth::user();
+        $data = [
+            'post_id'=> $request->post_id,
+            'author'=> $user->name,
+            'email' => $user->email,
+            'photo' => $user->photo->file,
+            'body' => $request->body
+        ];
+       Comment::create($data);
+       $request->session()->flash('comment_message', 'Your message has been submitted and is waiting for moderation');
+       return redirect()->back();
     }
 
     /**
