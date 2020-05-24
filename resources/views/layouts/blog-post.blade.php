40748ca1fb1d1e2e5cf0b5fd5705b901d0d97bfe
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-24 21:38:41 +0600

    Comments - Displaying replies with Javascript
    # Error  caused which is not showed in tutorial
    
    # fixed it
    
    # used <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>     in   post.blade.php file
    
    # reference  https://stackoverflow.com/questions/2075337/uncaught-referenceerror-is-not-defined

--- resources/views/layouts/blog-post.blade.php
+++ resources/views/layouts/blog-post.blade.php
@@ -150,6 +150,8 @@
 <!-- jQuery -->
 <script src="{{asset('js/libs.js')}}"></script>
 
+@yield('scripts')
+
 </body>
 
 </html>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -72,64 +72,75 @@
     <!-- Comment -->
 
     @if (count($comments)> 0)
-        
-    @foreach ($comments as $comment)
-        <div class="media">
-            <a class="pull-left" href="#">
-            <img height="64" class="media-object img-rounded" src=" {{ $comment->photo }} " alt="">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading">{{$comment->author}}
-                    <small>{{ $comment->created_at->diffForHumans() }}</small>
-                </h4>
-                <p> {{ $comment->body }} </p>
-
-
-                @if (count($comment->replies)> 0)
-
-                @foreach ($comment->replies as $reply)
-                    
-               
-
-           <!-- Nested Comment -->
-            <div id="nested-comment" class="media">
+        @foreach ($comments as $comment)
+            <div class="media">
                 <a class="pull-left" href="#">
-                <img height="34" class="media-object img-rounded" src=" {{ $reply->photo }}" alt="">
+                <img height="64" class="media-object img-rounded" src=" {{ $comment->photo }} " alt="">
                 </a>
+
                 <div class="media-body">
-                    <h4 class="media-heading"> {{$reply->author}}
-                    <small> {{$reply->created_at->diffForHumans()}} </small>
+                    <h4 class="media-heading">{{$comment->author}}
+                        <small>{{ $comment->created_at->diffForHumans() }}</small>
                     </h4>
-                    <p> {{$reply->body}} </p>
+                    <p> {{ $comment->body }} </p>
+
+
+                    @if (count($comment->replies)> 0)
+                        @foreach ($comment->replies as $reply)
+                                    
+                            <!-- Nested Comment -->
+                            <div id="nested-comment" class="media">
+                                <a class="pull-left" href="#">
+                                <img height="34" class="media-object img-rounded" src=" {{ $reply->photo }}" alt="">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="media-heading"> {{$reply->author}}
+                                    <small> {{$reply->created_at->diffForHumans()}} </small>
+                                    </h4>
+                                    <p> {{$reply->body}} </p>
+                                </div>
+
+                                <div class="comment-reply-container"> 
+                                    <button class="toggle-reply btn btn-primary pull-right">Reply</button>
+
+                                    <div class="comment-reply col-sm-6">
+                                        {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
+                                            
+                                            <div class="form-group">
+                                                <input type="hidden" name="comment_id" value="{{$comment->id}}">
+                                                {!! Form::label('body', 'Body:') !!}
+                                                {!! Form::textarea('body', null, ['class'=> 'form-control', 'rows'=>1]) !!}
+                                            </div>
+                                            <div class="form-group">
+                                                {!! Form::submit('submit', ['class'=> 'btn btn-primary']) !!}
+                                            </div>
+                                            {!! Form::close() !!}
+                                    </div>
+                                </div>
+                               <!-- End Nested Comment -->
+                            </div> 
+
+                        @endforeach
+                    @endif
+
                 </div>
-
-
-                {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
-                
-                <div class="form-group">
-                    <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                    {!! Form::label('body', 'Body:') !!}
-                    {!! Form::textarea('body', null, ['class'=> 'form-control', 'rows'=>1]) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::submit('submit', ['class'=> 'btn btn-primary']) !!}
-                </div>
-                {!! Form::close() !!}
-
-
             </div>
-            <!-- End Nested Comment -->
-
-            @endforeach
-
-            @endif
-
-
-            </div>
-        </div>
-    @endforeach
-
+        @endforeach
     @endif
 
+@stop
 
+@section('scripts')
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <script>
+
+        $(".comment-reply-container .toggle-reply").click(function(){
+            $(this).next().slideToggle("slow");
+        });
+
+    </script>
+    
 @stop
