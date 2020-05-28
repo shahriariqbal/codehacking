ada048844a82f6aa11227ac80d805091361d0aa5
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-28 20:00:00 +0600

    Upgrade to Laravel 5.3 - Upgrading
    # upgraded version 5.2.45 to 5.3.31

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'sfdsfsd';
+});
+
+Route::get('/admin', function () {
+
+    return view('admin.index');
+    
+});
