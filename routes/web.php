409e18acdff92baf28fd3e77e72ae52a483c1203
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-28 21:48:46 +0600

    Upgrade to Laravel 5.3 - Upgrading routes-Login, Route names

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,74 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
-
 Route::get('/', function () {
-    return 'sfdsfsd';
+    return view('welcome');
 });
 
-Route::get('/admin', function () {
+Route::auth();
+Route::get('/logout', 'Auth\LoginController@logout');
 
-    return view('admin.index');
-    
+
+
+Route::group(['middleware' => 'admin'], function () {
+
+    Route::get('/admin', function () {
+
+        return view('admin.index');
+        
+    });
+
+    Route::resource('admin/users', 'AdminUsersController', ['names'=> [
+
+        'index'=> 'admin.users.index',
+        'create'=> 'admin.users.create',
+        'store'=> 'admin.users.store',
+        'edit'=> 'admin.users.edit'
+
+    ]]);
+
+    Route::resource('admin/posts', 'AdminPostsController', ['names' => [
+
+        'index'=> 'admin.posts.index',
+        'create'=> 'admin.posts.create',
+        'store'=> 'admin.posts.store',
+        'edit'=> 'admin.posts.edit'
+
+    ]]);
+
+    Route::resource('admin/categories', 'AdminCategoriesController', ['names'=> [
+
+        'index'=> 'admin.categories.index',
+        'create'=> 'admin.categories.create',
+        'store'=> 'admin.categories.store',
+        'edit'=> 'admin.categories.edit'
+
+    ]]);
+
+    Route::resource('admin/media', 'AdminMediasController', ['names'=> [
+
+        'index'=> 'admin.media.index',
+        'create'=> 'admin.media.create',
+        'store'=> 'admin.media.store',
+        'edit'=> 'admin.media.edit'
+
+    ]]);
+
+    Route::resource('admin/comments', 'PostCommentsController', ['names'=> [
+
+        'index'=> 'admin.comments.index',
+        'create'=> 'admin.comments.create',
+        'store'=> 'admin.comments.store',
+        'edit'=> 'admin.comments.edit'
+
+    ]]);
+
+    Route::resource('admin/comment/replies', 'CommentRepliesController', ['names'=> [
+
+        'index'=> 'admin.replies.index',
+        'create'=> 'admin.replies.create',
+        'store'=> 'admin.replies.store',
+        'edit'=> 'admin.replies.edit'
+        
+    ]]);
+        
 });
