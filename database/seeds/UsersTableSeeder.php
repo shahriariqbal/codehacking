499cc52431f0f881bc328bc8b5561688e4c1284f
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-30 17:08:02 +0600

    Extra feature-Laravel data seeding  -  Creating a simple seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'role_id'=> 2,
+            'is_active'=>1,
+            'email' => str_random(10). '@gmail.com',
+            'password' => bcrypt('secret')
+        ]);
+    }
+}
