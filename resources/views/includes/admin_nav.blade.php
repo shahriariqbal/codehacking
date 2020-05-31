b74784b4a9cb3616d2c4dc95bdb392b304a5f45d
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-31 19:21:58 +0600

    Extra-Adjustments and more Features - Making admin more modular

---
+++ resources/views/includes/admin_nav.blade.php
@@ -0,0 +1,15 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+
+
+
+
+{{-- Admin Top Navigation --}}
+@include('includes.admin_top_nav')
+
+{{-- Admin Side Navigation --}}
+
+@include('includes.admin_side_nav')
+
+        <!-- /.navbar-static-side -->
+    </nav>
