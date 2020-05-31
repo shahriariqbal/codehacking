5471688e617db19ccc5172924b79d9268ae443af
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-05-31 20:15:44 +0600

    Extra-Adjustments and more Features  -  Some adjustments to admin posts page

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,14 +5,15 @@
 <h1> Posts </h1>
 
 
-<table class="table">
+<table class="table ">
     <thead>
       <tr>
         <th>Id</th>
         <th>Photo</th>
+        <th>Title</th>
         <th>Owner</th>
         <th>Category</th>
-        <th>Title</th>
+        
         <th>Body</th>
         <th>Post Link</th>
         <th>Comments</th>
@@ -28,9 +29,10 @@
            <tr>
            <td> {{$post->id}}</td>
            <td>  <img height="50" width="50" src="{{ $post->photo ? $post->photo->file : 'http://placehold.it/50x50'}}" alt="" class="img-responsive img-rounded"> </td>
-           <td> <a href="{{route('admin.posts.edit',$post->id)}}"> {{$post->user->name}} </a>  </td>
+           <td>  <a href="{{route('admin.posts.edit',$post->id)}}"> {{$post->title}}  </a>  </td>
+           <td>  {{$post->user->name}}  </td>
            <td> {{$post->category ? $post->category->name : 'Uncategorized'}} </td>
-           <td> {{$post->title}}</td>
+           
            <td> {{str_limit(  $post->body, 30) }} </td>
            <td><a href="{{route('home.post', $post->id)}}"> View Post</a></td>
            <td><a href="{{route('admin.comments.show', $post->id)}}"> View Comments</a></td>
